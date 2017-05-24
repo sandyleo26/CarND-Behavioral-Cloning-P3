3245511759180f55507e9d50a583b5d950a4a6a7
Sam Leo <sandyleo26@gmail.com>
2017-05-24 22:23:32 +1000

    use lenet

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Activation, Flatten, Input
+from keras.layers import Dense, Dropout, Activation, Flatten, Input, Lambda
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 import csv
@@ -8,6 +8,12 @@ import numpy as np
 
 BATCH_SIZE = 128
 NB_EPOCH = 10
+
+nb_filters_conv1 = 32
+nb_filters_conv2 = 64
+
+kernel_size = (5, 5)
+pool_size = (2, 2)
 
 images = []
 angles = []
@@ -28,8 +34,34 @@ y_train = np.array(angles)
 print(y_train.shape)
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+# preprocessing
+model.add(Lambda(lambda x: x / 255, input_shape=(160,320,3)))
+
+# conv1
+model.add(Convolution2D(nb_filters_conv1, kernel_size[0], kernel_size[1], border_mode='valid'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+
+# conv2
+model.add(Convolution2D(nb_filters_conv2, kernel_size[0], kernel_size[1]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=pool_size))
+model.add(Dropout(0.5))
+model.add(Flatten())
+
+# FC1
+model.add(Dense(4096))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+# FC2
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+# Output
 model.add(Dense(1))
+
 model.compile(optimizer='adam', loss='mse')
 model.fit(X_train, y_train, batch_size=BATCH_SIZE, nb_epoch=NB_EPOCH, verbose=1, validation_split=0.2)
 model.save('model.h5')
